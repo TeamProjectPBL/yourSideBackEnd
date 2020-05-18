62cb70412ee2dad00de213d00237df71cdf6482c
Olha <8unicorn89@gmail.com>
2020-05-18 20:58:42 +0200

    added skeleton and gitignore file

---
+++ src/test/java/com/pbl/yourside/YourSideApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pbl.yourside;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YourSideApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
